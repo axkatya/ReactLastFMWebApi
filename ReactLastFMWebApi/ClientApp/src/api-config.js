2f37419b6f43dfa0f87a5dcf9aa74b8a175ae473
Katsiaryna_Auchynnikava <Katsiaryna_Auchynnikava@epam.com>
2018-08-03 10:40:05 +0300

    Fix urls depend on env.

--- ReactLastFMWebApi/ClientApp/src/api-config.js
+++ ReactLastFMWebApi/ClientApp/src/api-config.js
@@ -1,11 +1,6 @@
-﻿let backendHost;
-
-const hostname = window && window.location && window.location.hostname;
-
-if(hostname.includes('localhost')) {
-  backendHost = 'https://localhost:44334';
-} else {
-  backendHost = 'https://lastfmreactwebapi.azurewebsites.net';
-}
+﻿
+const backendHost = (process.env.NODE_ENV === 'development') 
+    ? 'https://localhost:44334'
+    : 'https://lastfmreactwebapi.azurewebsites.net';
 
 export const API_ROOT = backendHost;
